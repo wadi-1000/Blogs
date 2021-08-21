c1603e4a2ab71d5d069547b334b88e1913a4c12a
wadi-1000 <joy.kiranga@student.moringaschool@gmail.com>
2021-08-21 07:37:04 +0300

    Added docstrings to all views for documentation

--- app/main/views.py
+++ app/main/views.py
@@ -5,17 +5,29 @@ from . import main
 # Views
 @main.route('/')
 def index():
+    '''
+    View function that returns homepage
+    '''
     return render_template('index.html')
 
 
 @main.route('/about')
 def about():
+    '''
+    View function that returns about page
+    '''
     return render_template('about.html')
 
 @main.route('/post')
 def post ():
+    '''
+    View function that returns postt page for posting blogs
+    '''
     render_template('post.html')
 
 @main.route('/contact')
 def contact():
+    '''
+    View function that returns contact page
+    '''
     return render_template('contact.html')
