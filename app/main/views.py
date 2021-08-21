ff835a87b81e3e80239731504600882c18ecc3c2
wadi-1000 <joy.kiranga@student.moringaschool@gmail.com>
2021-08-21 07:27:16 +0300

    Created view for homepage

---
+++ app/main/views.py
@@ -0,0 +1,13 @@
+from flask import render_template
+from . import main
+
+
+# Views
+@main.route('/')
+def index():
+    return render_template('index.html')
+
+
+# @main.route('/about')
+# def about():
+#     return render_template
