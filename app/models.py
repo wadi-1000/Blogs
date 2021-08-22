cc9e794e336e6d865cb90db23716a9ced155866c
wadi-1000 <joy.kiranga@student.moringaschool@gmail.com>
2021-08-22 16:14:28 +0300

    Created user table to store user data

--- app/models.py
+++ app/models.py
@@ -1,10 +1,33 @@
-from flask_sqlalchemy import SQLAlchemy 
+from werkzeug.security import generate_password_hash,check_password_hash
 from datetime import datetime
+from . import db
 
 class Blogpost(db.Model):
+    __tablename__ = 'blogs'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(50))
     subtitle = db.Column(db.String(50))
     author = db.Column(db.String(20))
-    date_posted = db.Column(db.DateTime)
+    date_posted = db.Column(db.DateTime,default=datetime.utcnow)
     content = db.Column(db.Text)
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer,primary_key = True)
+    username = db.Column(db.String(255))
+    pass_secure = db.Column(db.String(255))
+
+
+      @property
+        def password(self):
+            raise AttributeError('You cannot read the password attribute')
+
+        @password.setter
+        def password(self, password):
+            self.pass_secure = generate_password_hash(password)
+
+
+        def verify_password(self,password):
+            return check_password_hash(self.pass_secure,password)
+    def __repr__(self):
+        return f'User {self.username}'
